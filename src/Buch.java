ab972de8fdb6c7792e07793e0948552a3361ca3c
julianleinter123 <julianleitner@tsn.at>
2025-09-22 19:05:36 +0200

    Neue Klasse Buch implementiert

---
+++ src/Buch.java
@@ -0,0 +1,23 @@
+public class Buch extends Medium{
+
+    private String isbn;
+
+    public Buch(String titel, double wert, String standort, String isbn) {
+        super(titel, wert, standort);
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public void anzeigen() {
+        super.anzeigen();
+        System.out.println("[Buch]" + "Isbn: " + this.getIsbn());
+    }
+}
